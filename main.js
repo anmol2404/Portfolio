c98bf560d4b5bf8cccf3f8e1927a5d50144f4249
anmol2404 <sharma2404anmol@outlook.com>
2025-03-08 22:31:37 +0530

    Initail commit'

---
+++ main.js
@@ -0,0 +1,13 @@
+// function setBodyPadding() {
+//     const header = document.getElementById('header').offsetHeight;
+//     const footer = document.getElementById('footer').offsetHeight;
+
+//     document.getElementById('mainsection').style.paddingTop = header + 'px';
+//     document.getElementById('mainsection').style.paddingBottom = footer + 'px';
+// }
+
+// window.addEventListener('load', setBodyPadding);
+// window.addEventListener('resize', setBodyPadding);
+
+
+
